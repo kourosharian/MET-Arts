46c0bd4e37c164473f9bd84de82a34f85bf5c4a5
Mahdi Abolfazli <abolfazli.mahdi@gmail.com>
2023-01-31 17:21:12 +0330

    Implemented SearchFragment basic functionality

--- app/src/main/java/abolfazli/mahdi/metarts/arts/ArtsAdapter.kt
+++ app/src/main/java/abolfazli/mahdi/metarts/arts/ArtsAdapter.kt
@@ -1,4 +1,45 @@
 package abolfazli.mahdi.metarts.arts
 
-class ArtsAdapter {
+import abolfazli.mahdi.metarts.R
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import timber.log.Timber
+
+class ArtsAdapter: ListAdapter<Int, ArtsAdapter.ArtsViewHolder>(
+    object : DiffUtil.ItemCallback<Int>() {
+        override fun areItemsTheSame(oldItem: Int, newItem: Int): Boolean {
+            return oldItem == newItem
+        }
+        override fun areContentsTheSame(oldItem: Int, newItem: Int): Boolean {
+            return oldItem == newItem
+        }
+    }
+) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtsViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(android.R.layout.simple_list_item_1
+                , parent, false)
+        return ArtsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ArtsViewHolder, position: Int) {
+        val id = getItem(position)
+        holder.bind(id)
+    }
+
+    class ArtsViewHolder(itemView: View) : ViewHolder(itemView) {
+
+        fun bind(id: Int) {
+            Timber.e(id.toString())
+            (itemView as TextView).text = id.toString()
+
+        }
+    }
 }
---
+++ data/src/main/java/abolfazli/mahdi/data/arts/responses/SearchResponse.kt
@@ -0,0 +1,7 @@
+package abolfazli.mahdi.data.arts.responses
+
+data class SearchResponse(
+    val total: Int,
+    val objectIDs: List<Int>
+) {
+}
